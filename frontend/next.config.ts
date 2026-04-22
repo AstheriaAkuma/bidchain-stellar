4d6221402b1e151cbe9a1c6cff2ec30474215b2a
Nathalia Mancilla <nathaliamancilla30@gmail.com>
2026-04-22 11:29:54 +0800

    feat: update next.config.ts to include serverExternalPackages

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["@stellar/stellar-sdk", "@stellar/freighter-api"],
 };
 
 export default nextConfig;
